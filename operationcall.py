5aa75e8ddf800e40e9060256d89dd85170d5f346
vaishnavighodake <89295147+vaishnavighodake@users.noreply.github.com>
2021-08-29 16:25:40 +0530

    Add files via upload

---
+++ operationcall.py
@@ -0,0 +1,15 @@
+from Area.Circle.arcircle import areaofcircle
+from Area.Hexagon.arhexagon import areaofhexagon
+from Area.Oval.aroval import areaofoval
+from Area.Pentagon.arpentagon import areaofpentagon
+from Area.Rectangle.arrectangle import areaofrectangle
+from Area.Trapezium.artrapezium import areaoftrapezium
+from Area.Triangle.artriangle import areaoftriangle
+
+areaofcircle(5)
+areaofhexagon(8, 7)
+areaofoval(3, 5)
+areaofpentagon(18, 15)
+areaofrectangle(5, 4)
+areaoftrapezium(9, 10, 8)
+areaoftriangle(18, 6)
